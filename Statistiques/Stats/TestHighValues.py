88290a118028b0644b3a9335488b920fcbd4f965
Emilien <emilien.pons@viacesi.fr>
2019-07-04 12:24:24 +0200

    MAJ Correctif
    /

--- Statistiques/Stats/TestHighValues.py
+++ Statistiques/Stats/TestHighValues.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from collections import Counter
 from pylab import *
 from scipy import stats
 
@@ -10,8 +9,8 @@ machines = []
 
 nombreFichiers = 0
 # Remplacer le 4 par le nombre de de fichiers lu (+1)
-for i in range(1, 6):
-    chaine = "C:\\Users\\emili\\Desktop\\project\\tests\\analyse\\HighExecutionCities\\Dataset" + repr(
+for i in range(80, 110):
+    chaine = "C:\\Users\\emili\\Desktop\\project\\tests\\analyse\\datasets\\Dataset" + repr(
         i) + ".xlsx"
     print(chaine)
     rglobals.append(pd.read_excel(chaine, sheet_name='Params'))
@@ -22,74 +21,43 @@ for i in range(1, 6):
 tNB_CITY = []
 tNB_TRUCKS = []
 tDISTANCE = []
+tCoolRate = []
 
 for rglobal in rglobals:
     tNB_CITY.append(rglobal['Value'][0])
     tNB_TRUCKS.append(float(rglobal['Value'][1]))
     tDISTANCE.append(float(rglobal['Value'][6]))
+    tCoolRate.append(float(rglobal['Value'][8]))
+
 # On récupère notre temps (Axe X)
 tResolvedTime = []
 for machine in machines:
     tResolvedTime.append(machine['Value'][0])
 
+var = []
+def predict(var):
+    for v in range(0, len(var)):
+       var[v] = var[v] * slope + intercept
+    return var
 
-def predict(X):
-    for v in range(0, len(X)):
-        X[v] = X[v] * slope + intercept
-    return X
 
-
-'''
-print("===========GRAND NOMBRE CITIES===============")
-X = tNB_CITY
+print("===========GRAND [SOMETHING]===============")
+# Remplacer X = à ce que vous voulez mesurer
+# tNB_CITY
+# tNB_TRUCKS
+# tDISTANCE
+X = tCoolRate
+for i in X:
+    var.append(i)
 Y = tResolvedTime
-slope, intercept, r_value, p_value, std_err = stats.linregress(X, Y)  # Regression linéaire
-fitLine = predict(X)
+slope, intercept, r_value, p_value, std_err = stats.linregress(var, Y)  # Regression linéaire
+fitLine = predict(var)
 axes = plt.axes()
 axes.grid()
 plt.scatter(X, Y)  # Nuage de point
-plt.title('High Cities Number')
-plt.xlabel('nb Cities')
+plt.title('High Cool Number')
+plt.xlabel('nb Cool')
 plt.ylabel('Exec Time')
 plt.plot(X, fitLine, c='r')
 plt.show()
-print(tNB_CITY)
-print(tResolvedTime)
 print("OK")
-'''
-
-'''
-print("===========GRAND NOMBRE DISTANCE===============")
-X = tDISTANCE
-Y = tResolvedTime
-slope, intercept, r_value, p_value, std_err = stats.linregress(X, Y)  # Regression linéaire
-fitLine = predict(X)
-axes = plt.axes()
-axes.grid()
-plt.scatter(X, Y)  # Nuage de point
-plt.title('High Trucks number')
-plt.xlabel('nb Cities')
-plt.ylabel('Exec Time')
-plt.plot(X, fitLine, c='r')
-plt.show()
-print(tDISTANCE)
-print(tResolvedTime)
-print("OK")
-'''
-
-print("===========GRAND NOMBRE TRUCKS===============")
-X = tNB_TRUCKS
-Y = tResolvedTime
-slope, intercept, r_value, p_value, std_err = stats.linregress(X, Y)  # Regression linéaire
-fitLine = predict(X)
-axes = plt.axes()
-axes.grid()
-plt.scatter(X, Y)  # Nuage de point
-plt.title('High Trucks number')
-plt.xlabel('nb Cities')
-plt.ylabel('Exec Time')
-plt.plot(X, fitLine, c='r')
-plt.show()
-print(tNB_TRUCKS)
-print(tResolvedTime)
-
